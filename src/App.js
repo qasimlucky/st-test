499f0d7b9692186ae02c0d2dca3687900b201281
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2022-11-19 22:27:23 +0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
+/* import "bootstrap/dist/css/bootstrap.css"; */
 
 
 // admin pages
